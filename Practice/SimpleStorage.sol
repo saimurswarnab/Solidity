1a237420020ae186fb330396f47e830bd403da5d
saimurswarnab <Saimur101@gmail.com>
2024-10-30 17:28:02 +0600

    Solidity

--- Practice/SimpleStorage.sol
+++ Practice/SimpleStorage.sol
@@ -1,6 +1,14 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7; //solidity version 
+pragma solidity ^0.8.18; //solidity version 
 
 contract SimpleStorage{
+    //Basic Type: boolean , uint , int ,address, bytes
+//    bool hasFavouriteNumber= true;
+      uint256 favouritNumber;//0 // favorite number get initialize 0, if no value is given.
+//    int256 favouriteint =-66;
+//    string favouriteNumberInText = "hello swarnab";
+//    address myAdress = 0xffb0445Ce0BcB5A0BD6aB5385ff4255f864EC955;
+//    bytes32 favoriteByte = "cat";  // 0x2asda
+
 
 }
