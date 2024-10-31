bd6607c6e311109839517be77340f29e787a6bf4
saimurswarnab <Saimur101@gmail.com>
2024-10-31 17:52:51 +0600

    solidity

--- Practice/SimpleStorage.sol
+++ Practice/SimpleStorage.sol
@@ -3,12 +3,15 @@ pragma solidity ^0.8.18; //solidity version
 
 contract SimpleStorage{
     //Basic Type: boolean , uint , int ,address, bytes
-//    bool hasFavouriteNumber= true;
       uint256 favouritNumber;//0 // favorite number get initialize 0, if no value is given.
-//    int256 favouriteint =-66;
-//    string favouriteNumberInText = "hello swarnab";
-//    address myAdress = 0xffb0445Ce0BcB5A0BD6aB5385ff4255f864EC955;
-//    bytes32 favoriteByte = "cat";  // 0x2asda
+
+      function store(uint256 _favouriteNumber) public{ //Making a function
+        favouritNumber = _favouriteNumber;
+      }
+
+      function retrieve() public view returns (uint256){ // if we don't want to use public in favourite number, then we can use another funtion for view like this
+        return favouritNumber;
+      }
 
 
 }
