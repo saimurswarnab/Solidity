e1931008abc1b5df39e5da4ac96871768282683c
saimurswarnab <Saimur101@gmail.com>
2024-11-10 19:02:59 +0600

    Solidity

--- Fund me/FundMe.sol
+++ Fund me/FundMe.sol
@@ -2,10 +2,12 @@
 pragma solidity ^0.8.17;
 
 contract FundMe{
+    uint256 public myValue = 1;
     function fund()public payable{
         //Allow users to send $
         //Have a minimum $ sent
         //1. How do we send ETH to the contract?
+        myValue = myValue+2;
 
         require(msg.value > 1e18, "didn't send enough eth"); //1e18 = 1 ETH = 1000000000000000000 = 1 * 10 ** 18 wei
 
@@ -13,5 +15,5 @@ contract FundMe{
 
     }
 
-    function withdraw()public{}
+    //function withdraw()public{}
 }
