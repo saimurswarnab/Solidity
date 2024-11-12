18dd41eab744182da327d806ef66b8f4fccb64f6
saimurswarnab <Saimur101@gmail.com>
2024-11-12 20:06:42 +0600

    Solidity

--- Fund me/FundMe.sol
+++ Fund me/FundMe.sol
@@ -1,13 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 contract FundMe{
-    uint256 public myValue = 1;
+
+    uint256 public minimumUsd = 5;
+
     function fund()public payable{
         //Allow users to send $
         //Have a minimum $ sent
         //1. How do we send ETH to the contract?
-        myValue = myValue+2;
 
         require(msg.value > 1e18, "didn't send enough eth"); //1e18 = 1 ETH = 1000000000000000000 = 1 * 10 ** 18 wei
 
@@ -16,4 +18,15 @@ contract FundMe{
     }
 
     //function withdraw()public{}
+
+    function getPrice()public view returns(uint256){
+        //Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        //ABI
+        AggregatorV3Interface pirceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (,int256 answer,,,) = pirceFeed.latestRoundData();
+        //price of ETH in term of USD
+        //200.00000000
+        return uint256(answer * 1e10);
+    }
+    function getCoversionRate()public{}
 }
